f047eea1912f80fdb22cbbb08c1eb91482a9b8d1
desheng.leng <1032346882@qq.com>
2018-08-11 15:04:44 +0800

    project init

---
+++ src/vueAppStore.js
@@ -0,0 +1,17 @@
+//state
+import Vue from 'vue';
+import Vuex from 'vuex';
+import StockManager from './services/stockManager.js';
+Vue.use(Vuex);
+const store = new Vuex.Store({
+  state: {
+
+  },
+  mutations: {
+    initData(){
+
+    }
+  }
+});
+export default store;
+
