67ada801d40ab7a17d3d0b4e560227611c6a39c7
desheng.leng <1032346882@qq.com>
2018-11-06 23:24:33 +0800

    step...

--- src/api.js
+++ src/api.js
@@ -1,123 +1,119 @@
 import axios from 'axios';
-import {klineConf,stockInfoConf} from './utils/klineMap';
+import { klineConf, stockInfoConf } from './utils/klineMap';
 axios.defaults.timeout = 6000;
 const resMap = klineConf.resMap;
 
-function getStockBasicInfo(codes){
-  axios.get(`/GetUrlResultProxy?url=${encodeURIComponent('https://stock.xueqiu.com/v5/stock/batch/quote.json?symbol=${codes}')}`).then((data)=>{
-
-  });
+function getStockBasicInfo(code) {
+  code = formatCode(code);
+  let url = `https://stock.xueqiu.com/v5/stock/batch/quote.json?symbol=${code}&extend=detail`;
+  return axios.get(`/GetUrlResultProxy?url=${encodeURIComponent(url)}`).then((res) =>
+    res.data.data.items.shift());
 }
 
-function startFilter(codes, dateOffset, progressListener) {
-  var cloneCodes = [];
-  var totalLength = codes.length;
-  Object.assign(cloneCodes, codes);
-  for (var i = 0; i < 20; i++) {
-    _s();
-  }
-  function _s() {
-    if (cloneCodes.length <= 0) return;
-    let code = cloneCodes.pop();
-    let formatCode = code.code;
-    if (/^600/.test(code.code)) formatCode = 'SH' + code.code;
-    if (/^601/.test(code.code)) formatCode = 'SH' + code.code;
-    if (/^603/.test(code.code)) formatCode = 'SH' + code.code;
-    //if (/^60/.test(code.code)) formatCode = 'SZ' + code.code;
-    if (/^300/.test(code.code)) formatCode = 'SZ' + code.code;
-    if (/^002/.test(code.code)) formatCode = 'SZ' + code.code;
-    if (/^000/.test(code.code)) formatCode = 'SZ' + code.code;
-    checkCode(formatCode, dateOffset).then((res) => {
-      progressListener({
-        upDownRate: res.upDownRate,
-        isGood: res.isGood,
-        code: code,
-        stockInfo: res.stockInfo,
-        isError: false,
-        totalLength: totalLength,
-        remainLength: cloneCodes.length
-      });
+function formatCode(code) {
+  let formatedCode;
+  if (/^600/.test(code)) formatedCode = 'SH' + code;
+  if (/^601/.test(code)) formatedCode = 'SH' + code;
+  if (/^603/.test(code)) formatedCode = 'SH' + code;
+  //if (/^60/.test(code.code)) formatCode = 'SZ' + code.code;
+  if (/^300/.test(code)) formatedCode = 'SZ' + code;
+  if (/^002/.test(code)) formatedCode = 'SZ' + code;
+  if (/^000/.test(code)) formatedCode = 'SZ' + code;
+  return formatedCode;
+}
+
+
+function stockFiterBuilder(codes) {
+  return {
+    startFilter,
+    watchProgcess
+  };
+
+  function startFilter(codes, dateOffset, progressListener) {
+    var cloneCodes = [];
+    var totalLength = codes.length;
+    Object.assign(cloneCodes, codes);
+    for (var i = 0; i < 20; i++) {
       _s();
-    }).catch((err) => {
-      console.error(`${code.code}读取数据出错！${err}`);
-      progressListener({
-        isGood: false,
-        isError: true,
-        code: code,
-        totalLength: totalLength,
-        remainLength: cloneCodes.length
+    }
+    function _s() {
+      if (cloneCodes.length <= 0) return;
+      let code = cloneCodes.pop();
+      let formatedCode = formatCode(code.code);
+      checkCode(formatedCode, dateOffset).then((res) => {
+        progressListener({
+          upDownRate: res.upDownRate,
+          isGood: res.isGood,
+          code: code,
+          stockInfo: res.stockInfo,
+          isError: false,
+          totalLength: totalLength,
+          remainLength: cloneCodes.length
+        });
+        _s();
+      }).catch((err) => {
+        console.error(`${code.code}读取数据出错！${err}`);
+        progressListener({
+          isGood: false,
+          isError: true,
+          code: code,
+          totalLength: totalLength,
+          remainLength: cloneCodes.length
+        });
+        _s();
       });
-      _s();
-    });
+    }
+
+    function getKline(code, beginTime, type) {
+      var count = -2;
+      var apiUrl = klineConf.getApiUrl(code, type, count, beginTime);
+      return axios.get(`/GetUrlResultProxy?url=${encodeURIComponent(apiUrl)}`);
+    }
+
+    function checkCode(code, dateOffset) {
+      return getKline(code, dateOffset, 'week').then((res) => {
+        let data = res.data.data;
+        if (!data.item) {
+          return Promise.reject(`${code}返回数据有误，请检查！`);
+        }
+        let lastItem = data.item.pop();
+        let olderItem_1 = data.item.pop();
+        var result = false;
+        var macd_0 = lastItem[resMap.macd];
+        var macd_1 = olderItem_1[resMap.macd];
+
+        var percent_0 = lastItem[resMap.percent];
+
+        if (
+          macd_0 > macd_1
+          && macd_0 < 0
+          && percent_0 < 0.5
+        ) {
+          result = true;
+        }
+        return Promise.resolve({
+          isGood: result,
+          stockInfo: lastItem
+        });
+      }).then(res => {
+        return new Promise((resolve, reject) => {
+          getKline(code, dateOffset, 'day').then(res => {
+
+          });
+        });
+      });
+    }
   }
 
-  function getKline(code, beginTime) {
-    var count = -5;
-    if (beginTime + 3600000 * 15 >= (new Date()).getTime()) count = -4;
-    var apiUrl = klineConf.getApiUrl(code,'week',count,beginTime);
-    var p1 = axios.get(`/GetUrlResultProxy?url=${encodeURIComponent(apiUrl)}`);
-    return p1;
-  }
-  
-  function checkCode(code, dateOffset) {
-    return getKline(code, dateOffset).then((res) => {
-      let data = res.data.data;
-      if (!data.item) {
-        return Promise.reject(`${code}返回数据有误，请检查！`);
-      }
-      let lastItem = {};
-      if (data.item.length == 5) lastItem = data.item.pop();
-      let olderItem_1 = data.item.pop();
-      let olderItem_2 = data.item.pop();
-      let olderItem_3 = data.item.pop();
-      let olderItem_4 = data.item.pop();
-      var result = false;
-      //var mainHeight = olderItem_1[resMap.closePrice] - olderItem_1[resMap.openPrice];
-      var mainHeightAbs = Math.abs(olderItem_1[resMap.closePrice] - olderItem_1[resMap.openPrice]);
-      //var olderMainHeight = olderItem_2[resMap.closePrice] - olderItem_2[resMap.openPrice];
-      var upHeight = olderItem_1[resMap.highPrice] - Math.max(olderItem_1[resMap.openPrice], olderItem_1[resMap.closePrice]);
-      var downHeight = Math.min(olderItem_1[resMap.openPrice], olderItem_1[resMap.closePrice]) - olderItem_1[resMap.lowPrice];
-  
-      var axios_1 = (olderItem_1[resMap.highPrice] - olderItem_1[resMap.lowPrice]) / 2 + olderItem_1[resMap.lowPrice];
-      var axios_2 = (olderItem_2[resMap.highPrice] - olderItem_2[resMap.lowPrice]) / 2 + olderItem_2[resMap.lowPrice];
-      var axios_3 = (olderItem_3[resMap.highPrice] - olderItem_3[resMap.lowPrice]) / 2 + olderItem_3[resMap.lowPrice];
-      var axios_4 = (olderItem_4[resMap.highPrice] - olderItem_4[resMap.lowPrice]) / 2 + olderItem_4[resMap.lowPrice];
-  
-  
-      var weekMat20_1 = olderItem_1[resMap.ma20];
-      var weekMat20_2 = olderItem_2[resMap.ma20];
-      var weekMat20_3 = olderItem_3[resMap.ma20];
-      var weekMat20_4 = olderItem_4[resMap.ma20];
-  
-      var upDownRate = upHeight / downHeight;
-      if (
-        weekMat20_1 >= weekMat20_2
-        && weekMat20_2 <= weekMat20_3
-        //upDownRate > 2 
-        //upHeight + downHeight < mainHeightAbs
-        //axios_1 < axios_2
-        //&& axios_2 > axios_3
-        //&& axios_3 < axios_4
-        /*olderItem_1[9] <= olderItem_2[9]
-        && olderItem_2[9] >= olderItem_3[9]
-        && olderItem_3[9] <= olderItem_4[9]
-        && olderItem_2[9] < olderItem_4[9]
-        && olderItem_1[9] > olderItem_3[9]*/
-      //&& 2 * mainHeightAbs < upHeight
-      //&& olderItem_2[7] < 0
-      //&& olderItem_1[7] < 0
-      ) {
-        result = true;
-      }
-      return Promise.resolve({
-        isGood: result,
-        stockInfo: lastItem,
-        upDownRate
-      });
-    });
+  function watchProgcess() {
+
   }
 }
+
+
 
 export default {
-  startFilter
+  stockFiterBuilder,
+  formatCode,
+  getStockBasicInfo
 };
--- src/components/VueStockList.vue
+++ src/components/VueStockList.vue
@@ -5,54 +5,68 @@
     <el-button @click="startFilter" type="primary">start</el-button>
     <div>{{totalLength - remainLength}}/{{totalLength}}</div>
     <el-progress :text-inside="true" :stroke-width="18" :percentage="filterProgress" color="rgba(142, 113, 0, 1)"></el-progress>
-    <el-table :data="tableData" style="width: 100%" :default-sort = "{prop: 'increase', order: 'descending'}">
+    <el-table :data="tableData" style="width: 100%" :default-sort = "{prop: 'capital', order: 'descending'}">
         <el-table-column prop="companyName" label="公司名称">
         </el-table-column>
         <el-table-column prop="pirce" label="价格">
         </el-table-column>
         <el-table-column prop="increase" sortable label="涨幅">
         </el-table-column>
-        <el-table-column prop="upDownRate" sortable label="上下比例">
+        <el-table-column prop="capital" sortable label="市值">
+        </el-table-column>
+        <el-table-column prop="pe" sortable label="市盈率">
         </el-table-column>
     </el-table>
 </div>
 </template>
 
   <script>
-import codes from '../stocks';
+import codes from "../stocks";
 export default {
-    data() {
-        return {
-            selectedDate: '',
-            tableData: [],
-            filterProgress: 0,
-            totalLength: 0,
-            remainLength: 0
-        }
-    },
-    methods: {
-        startFilter() {
-            var selectedDate = (new Date(this.selectedDate)).getTime();
-            var dateOffset = selectedDate;
+  data() {
+    return {
+      selectedDate: "",
+      tableData: [],
+      filterProgress: 0,
+      totalLength: 0,
+      remainLength: 0
+    };
+  },
+  methods: {
+    startFilter() {
+      var selectedDate = new Date(this.selectedDate).getTime();
+      var dateOffset = selectedDate;
 
-            api.startFilter(codes,dateOffset, (result) => {
-                this.totalLength = result.totalLength;
-                this.remainLength = result.remainLength;
-                this.filterProgress = 100 - parseInt(result.remainLength * 100 / result.totalLength);
-                if (result.isGood && (selectedDate - result.stockInfo[0])<3600000 * 7 * 24) {
-                    this.tableData.push({
-                        companyName: result.code.name,
-                        pirce: result.stockInfo[5],
-                        increase: result.stockInfo[7] + "%",
-                        upDownRate:result.upDownRate.toFixed(2)
-                    });
-                }
+      api.stockFiterBuilder().startFilter(codes, dateOffset, result => {
+        this.totalLength = result.totalLength;
+        this.remainLength = result.remainLength;
+        this.filterProgress =
+          100 - parseInt((result.remainLength * 100) / result.totalLength);
+        if (
+          result.isGood &&
+          selectedDate - result.stockInfo[0] < 3600000 * 14 * 24
+        ) {
+          api
+            .getStockBasicInfo(result.code.code)
+            .then(data =>
+              Promise.resolve({
+                capital: data.quote.market_capital / 100000000,
+                pe_lyr: data.quote.pe_lyr
+              })
+            )
+            .then(data => {
+              this.tableData.push({
+                companyName: result.code.name,
+                pirce: result.stockInfo[5],
+                increase: result.stockInfo[7] + "%",
+                capital: data.capital,
+                pe: data.pe_lyr
+              });
             });
-
         }
-    },
-    mounted() {
-
+      });
     }
-}
+  },
+  mounted() {}
+};
 </script>
